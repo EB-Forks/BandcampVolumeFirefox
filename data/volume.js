957609834231cba0df2e6c4ce10686987c5b7099
Daniel Kamkha <daniel.kamkha@gmail.com>
2015-05-16 10:31:44 +0300

    Merged volume-extras.js into volume.js

--- data/volume.js
+++ data/volume.js
@@ -1,96 +1,110 @@
-//     This file is part of Bandcamp Volume extension for Google Chrome
+//     This file is part of Bandcamp Volume add-on for Mozilla Firefox
+//     https://github.com/DanielKamkha/bandcamp_volume_firefox
+//     (c) 2015 Daniel Kamkha
+//     Based on Bandcamp Volume extension for Google Chrome
 //     https://github.com/ubercow/bandcamp_volume
 //     (c) 2013 Zak Kristjanson
 //     Bandcamp Volume is free software distributed under the terms of the MIT license.
 
-var BandcampVolume =
-{
-	_ranges:[],
-	_audiotag:null,
-	_slider_change:function(evt)
-	{
-		// Set the audio tag's volume to the slider's volume
-		this._audiotag.volume = evt.target.value
-	},
-	_slider_mouseup:function(evt)
-	{
-		// Get the value of the input
-		var newvol = evt.target.value
+(function() {
+  "use strict";
 
-		// Put it in Chrome's local storage for global persistance
-		chrome.storage.local.set({"volume":newvol})
+  let BandcampVolume =
+  {
+    _ranges: [],
+    _audioTag: null,
+    _rowspanRow: null,
+    _sliderRow: null,
+    _slider_change: function(evt) {
+      // Set the audio tag's volume to the slider's volume
+      this._audioTag.volume = evt.target.value
+    },
+    _slider_mouseup: function(evt) {
+      // Get the value of the input
+      var newVol = evt.target.value;
 
-		// Yes I know I am going to set the slider that's triggering this event to it's own value.
-		this._ranges.forEach(function(element, index, array)
-		{
-			element.value = newvol
-		});
-	},
-	load:function()
-	{
-		this._audiotag = document.getElementsByTagName("audio")[0]
+      // Put it in Chrome's local storage for global persistence
+      chrome.storage.local.set({"volume": newVol});
 
-		// Rebind this for use in the callback
-		var bcv = this
-		chrome.storage.local.get("volume", function(items) {
-			var newvol = items["volume"] || bcv._audiotag.volume
+      // Yes I know I am going to set the slider that's triggering this event to it's own value.
+      this._ranges.forEach( function(element) { element.value = newVol; });
+    },
+    load: function() {
+      this._audioTag = document.getElementsByTagName("audio")[0];
 
-			bcv._ranges.forEach(function(element, index, array)
-			{
-				element.value = newvol
-			});
-			bcv._audiotag.volume = newvol
-		});
+      var that = this;
+      chrome.storage.local.get("volume", function(items) {
+        var newVol = items["volume"] || that._audioTag.volume;
 
-		// Create the volume layout
-		var desktop_view = document.getElementsByClassName("inline_player")[0]
-		var audiotag = document.getElementsByTagName("audio")[0] 
-		desktop_view.querySelector("tr:first-child td:first-child").setAttribute("rowspan", "3")
+        that._ranges.forEach(function(element) { element.value = newVol; });
+        that._audioTag.volume = newVol
+      });
 
-		var row = document.createElement("tr")
-		var col = row.appendChild(document.createElement("td"))
-		col.setAttribute("colspan", "3")
+      // Create the volume layout
+      var desktop_view = document.getElementsByClassName("inline_player")[0];
+      this._rowspanRow = desktop_view.querySelector("tr:first-child td:first-child");
+      this._rowspanRow.setAttribute("rowspan", "3");
 
-		var volcontainer = document.createElement("div")
-		volcontainer.style.marginLeft = "0.83em"
+      this._sliderRow = document.createElement("tr");
+      var col = this._sliderRow.appendChild(document.createElement("td"));
+      col.setAttribute("colspan", "3");
 
-		var input = document.createElement("span")
-		input.innerHTML = "Volume:"
-		input.style.marginRight = "0.3em"
-		input.style.fontWeight = "bold"
-		input.style.display = "inline-block"
-		input.style.verticalAlign = "middle"
-		input.style.height = "14px"
+      var volContainer = document.createElement("div");
+      volContainer.style.marginLeft = "0.83em";
 
+      var volumeSpan = document.createElement("span");
+      volumeSpan.style.fontWeight = "bold";
+      volumeSpan.style.display = "inline-block";
+      volumeSpan.style.verticalAlign = "middle";
+      volumeSpan.style.height = "14px";
+      volumeSpan.className = "fa fa-volume-up fa-lg";
 
-		// Get some stuff from the player progress bar to add style to the volume bar
-		var playprogbar = desktop_view.querySelector(".progbar_empty")
-		var playprogbar_style = (playprogbar.currentStyle || window.getComputedStyle(playprogbar, null))
+      // Get some stuff from the player progress bar to add style to the volume bar
+      var playProgbar = desktop_view.querySelector(".progbar_empty");
+      var playProgbar_style = (playProgbar.currentStyle || window.getComputedStyle(playProgbar, null));
 
-		var range = document.createElement("input")
-		range.style.display = "inline-block"
-		range.style.verticalAlign = "middle"
+      var range = document.createElement("input");
+      range.style.display = "inline-block";
+      range.style.verticalAlign = "middle";
 
-		range.style.height = "4px"
-		range.style.webkitAppearance = "none"
-		range.style.background = "none"
-		range.style.border = playprogbar_style.border
-		range.style.outline = "none";
+      range.style.height = "4px";
+      range.style.webkitAppearance = "none";
+      range.style.background = "none";
+      range.style.border = playProgbar_style.border;
+      range.style.outline = "none";
 
-		range.type="range"
-		range.max = 1
-		range.step = 0.01
-		range.min = 0
-		range.value = this._audiotag.volume
-		range.addEventListener("change", this._slider_change.bind(this))
-		range.addEventListener("mouseup", this._slider_mouseup.bind(this))
-		this._ranges.push(range)
+      range.type="range";
+      range.max = 1;
+      range.step = 0.01;
+      range.min = 0;
+      range.value = this._audioTag.volume;
+      range.addEventListener("change", this._slider_change.bind(this));
+      range.addEventListener("mouseup", this._slider_mouseup.bind(this));
+      this._ranges.push(range);
 
-		volcontainer.appendChild(input)
-		volcontainer.appendChild(range)
-		col.appendChild(volcontainer)
-		desktop_view.querySelector("tbody").appendChild(row)
-	}
-}
+      volContainer.appendChild(volumeSpan);
+      volContainer.appendChild(range);
+      col.appendChild(volContainer);
+      desktop_view.querySelector("tbody").appendChild(this._sliderRow);
+    },
+    unload: function () {
+      if (this._sliderRow) {
+        this._sliderRow.parentNode.removeChild(this._sliderRow);
+        this._sliderRow = null;
+      }
+      if (this._rowspanRow) {
+        this._rowspanRow.setAttribute("rowspan", "2");
+        this._rowspanRow = null;
+      }
+    }
+  };
 
-BandcampVolume.load()
+  BandcampVolume.load();
+
+  self.port.on("detach", function () {
+    if (BandcampVolume) {
+      BandcampVolume.unload();
+      BandcampVolume = null;
+    }
+  });
+})();
