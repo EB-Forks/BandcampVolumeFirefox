439b9e49dcdba0c5b6b4303fc2045ccf15deddca
Daniel Kamkha <daniel.kamkha@gmail.com>
2015-06-02 20:20:33 +0300

    Refactored into ctor fcn

--- data/volume.js
+++ data/volume.js
@@ -9,87 +9,102 @@
 (function() {
   "use strict";
 
-  let rangesCache = [];
-  let audioTag = null;
-  let rowspanRow = null;
-  let sliderRow = null;
+  function BandcampVolume() {
+    let rangesCache = [];
+    let audioTag = null;
+    let rowspanRow = null;
+    let sliderRow = null;
 
-  function _slider_change(evt) {
-    // Set the audio tag's volume to the slider's volume
-    audioTag.volume = evt.target.value;
-  }
-
-  function _slider_mouseup(evt) {
-    // Get the value of the input
-    let newVol = evt.target.value;
-
-    // Put it in storage for global persistence
-    self.port.emit("set", {"volume": newVol});
-
-    // Yes I know I am going to set the slider that's triggering this event to it's own value.
-    rangesCache.forEach( function(element) { element.value = newVol; });
-  }
-
-  function doAttach() {
-    audioTag = document.getElementsByTagName("audio")[0];
-
-    self.port.once("get", function(items) {
-      let newVol = items["volume"] || audioTag.volume;
-      rangesCache.forEach(function(element) { element.value = newVol; });
-      audioTag.volume = newVol
-    });
-    self.port.emit("get");
-
-    // Create the volume layout
-    let desktop_view = document.getElementsByClassName("inline_player")[0];
-    rowspanRow = desktop_view.querySelector("tr:first-child td:first-child");
-    rowspanRow.setAttribute("rowspan", "3");
-
-    sliderRow = document.createElement("tr");
-    let col = sliderRow.appendChild(document.createElement("td"));
-    col.setAttribute("colspan", "3");
-
-    let volContainer = document.createElement("div");
-    volContainer.style.marginLeft = "0.83em";
-
-    let volumeIcon = document.createElement("span");
-    volumeIcon.className = "fa fa-volume-up fa-lg volumeIcon";
-
-    // Get some stuff from the player progress bar to add style to the volume bar
-    let playProgbar = desktop_view.querySelector(".progbar_empty");
-    let playProgbar_style = (playProgbar.currentStyle || window.getComputedStyle(playProgbar, null));
-
-    let volumeSlider = document.createElement("input");
-    volumeSlider.className = "volumeSlider";
-    volumeSlider.style.border = playProgbar_style.border;
-
-    volumeSlider.type = "range";
-    volumeSlider.max = 1;
-    volumeSlider.step = 0.01;
-    volumeSlider.min = 0;
-    volumeSlider.value = audioTag.volume;
-    volumeSlider.addEventListener("change", _slider_change);
-    volumeSlider.addEventListener("mouseup", _slider_mouseup);
-    rangesCache.push(volumeSlider);
-
-    volContainer.appendChild(volumeIcon);
-    volContainer.appendChild(volumeSlider);
-    col.appendChild(volContainer);
-    desktop_view.querySelector("tbody").appendChild(sliderRow);
-  }
-
-  function doDetach() {
-    if (sliderRow) {
-      sliderRow.parentNode.removeChild(sliderRow);
-      sliderRow = null;
+    function _slider_change(evt) {
+      // Set the audio tag's volume to the slider's volume
+      audioTag.volume = evt.target.value;
     }
-    if (rowspanRow) {
-      rowspanRow.setAttribute("rowspan", "2");
-      rowspanRow = null;
+
+    function _slider_mouseup(evt) {
+      // Get the value of the input
+      let newVol = evt.target.value;
+
+      // Put it in storage for global persistence
+      self.port.emit("set", {"volume": newVol});
+
+      // Yes I know I am going to set the slider that's triggering this event to it's own value.
+      rangesCache.forEach(function (element) {
+        element.value = newVol;
+      });
+    }
+
+    function attach() {
+      audioTag = document.getElementsByTagName("audio")[0];
+
+      self.port.once("get", function (items) {
+        let newVol = items["volume"] || audioTag.volume;
+        rangesCache.forEach(function (element) {
+          element.value = newVol;
+        });
+        audioTag.volume = newVol
+      });
+      self.port.emit("get");
+
+      // Create the volume layout
+      let desktop_view = document.getElementsByClassName("inline_player")[0];
+      rowspanRow = desktop_view.querySelector("tr:first-child td:first-child");
+      rowspanRow.setAttribute("rowspan", "3");
+
+      sliderRow = document.createElement("tr");
+      let col = sliderRow.appendChild(document.createElement("td"));
+      col.setAttribute("colspan", "3");
+
+      let volContainer = document.createElement("div");
+      volContainer.style.marginLeft = "0.83em";
+
+      let volumeIcon = document.createElement("span");
+      volumeIcon.className = "fa fa-volume-up fa-lg volumeIcon";
+
+      // Get some stuff from the player progress bar to add style to the volume bar
+      let playProgbar = desktop_view.querySelector(".progbar_empty");
+      let playProgbar_style = (playProgbar.currentStyle || window.getComputedStyle(playProgbar, null));
+
+      let volumeSlider = document.createElement("input");
+      volumeSlider.className = "volumeSlider";
+      volumeSlider.style.border = playProgbar_style.border;
+
+      volumeSlider.type = "range";
+      volumeSlider.max = 1;
+      volumeSlider.step = 0.01;
+      volumeSlider.min = 0;
+      volumeSlider.value = audioTag.volume;
+      volumeSlider.addEventListener("change", _slider_change);
+      volumeSlider.addEventListener("mouseup", _slider_mouseup);
+      rangesCache.push(volumeSlider);
+
+      volContainer.appendChild(volumeIcon);
+      volContainer.appendChild(volumeSlider);
+      col.appendChild(volContainer);
+      desktop_view.querySelector("tbody").appendChild(sliderRow);
+    }
+
+    function detach() {
+      if (sliderRow) {
+        sliderRow.parentNode.removeChild(sliderRow);
+        sliderRow = null;
+      }
+      if (rowspanRow) {
+        rowspanRow.setAttribute("rowspan", "2");
+        rowspanRow = null;
+      }
+    }
+
+    return {
+      attach: attach,
+      detach: detach
     }
   }
 
-  doAttach();
+  let controller = new BandcampVolume();
+  controller.attach();
 
-  self.port.on("detach", doDetach);
+  self.port.on("detach", function() {
+    controller.detach();
+    controller = null;
+  });
 })();
